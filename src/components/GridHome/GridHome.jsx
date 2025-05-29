075b4182825128da6fc7a21ed2bc352545717f23
David Sanchez <dasava11@outlook.com>
2025-05-29 06:51:24 -0500

    #1

--- src/components/GridHome/GridHome.jsx
+++ src/components/GridHome/GridHome.jsx
@@ -1,8 +1,35 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 export const GridHome = () => {
+  const [stores, setStores] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const URL = "/api"
+
+  useEffect(() => {
+    const fetchStores = async () => {
+      setLoading(true)
+      try {
+        const response = await axios.get(URL)
+        setStores(response.data)
+        console.log(response.data);
+      } catch (error) {
+        setError(error.message)
+        console.error("Error fetching stores:", error)
+        alert("Error fetching stores: " + error.message)
+      } finally {
+        setLoading(false)
+      } 
+    }
+    
+    fetchStores()
+  }, []);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      hola mundo
+      
     </div>
   )
 }
