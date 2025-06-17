a0f92434c07ca5503108067678bdf677754558e1
dasava11 <dasava11@outlook.com>
2025-06-16 20:19:25 -0500

    css global

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import MyRoutes from "./routes/routes";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center bg-primary-100 text-white font-primary min-h-screen text-center px-4">
-    {/*  <header className="mb-8">
+    <div className="flex flex-col items-center justify-center font-primary min-h-screen text-center px-4">
+      {/*  <header className="mb-8">
       
      </header> */}
       <MyRoutes />
--- src/main.jsx
+++ src/main.jsx
@@ -4,40 +4,40 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App.jsx";
 import "./index.css";
+import "./App.css";
 import clsx from "clsx";
 import { AuthProvider } from "./context/authContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  
   <StrictMode>
     <BrowserRouter>
-    <AuthProvider>
-      <App />
-      <ToastContainer
-      position="top-right"
-      autoClose={3000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      toastClassName={(context) =>
-        clsx(
-          "rounded-md px-4 py-3 font-medium shadow-lg transition-all duration-300",
-          {
-            "bg-success-500 text-white": context?.type === "success",
-            "bg-error-500 text-white": context?.type === "error",
-            "bg-warning-500 text-black": context?.type === "warning",
-            "bg-primary-500 text-white":
-            context?.type === "default" || context?.type === "info",
+      <AuthProvider>
+        <App />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          toastClassName={(context) =>
+            clsx(
+              "rounded-md px-4 py-3 font-medium shadow-lg transition-all duration-300",
+              {
+                "bg-success-500 text-white": context?.type === "success",
+                "bg-error-500 text-white": context?.type === "error",
+                "bg-warning-500 text-black": context?.type === "warning",
+                "bg-primary-500 text-white":
+                  context?.type === "default" || context?.type === "info",
+              }
+            )
           }
-        )
-      }
-      />
-    </AuthProvider>
+        />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
